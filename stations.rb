c6df9d69f4d994d93800f364bd89d960513c9e1a
Abdel A Saleh <asaleh@the-experimenters.com>
2009-10-05 13:32:42 -0400

    Updated the sample programme location.

--- stations.rb
+++ stations.rb
@@ -25,7 +25,7 @@ get '/stations/1/new_programme.xml' do
   content_type "text/xml"
   expected_programme = {
     "title" => "Tweeting the News",
-    "location" => "http://localhost:3000/sample_programme/dave_winer_tweeting_the_news.mp3",
+    "location" => "http://localhost:3000/sample_programme/test.mp3",
     "duration" => 2580000, # in milliseconds
     "info" => "http://itc.conversationsnetwork.org/shows/detail4147.html",
     "creator" => "Technometria with Phil Windley",
